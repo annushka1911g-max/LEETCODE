dd134e1dce0d94577be048448c4e3f8a83195ddb
Anushka Dwivedi <annushka1911g@gmail.com>
2026-07-25 20:27:26 +0530

    Time: 4 ms (81.1%), Space: 10.3 MB (98.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,31 +4,29 @@ public:
         int m = word1.size();
         int n = word2.size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
-        // Base cases
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = i;
+        vector<int> prev(n + 1), curr(n + 1);
 
         for (int j = 0; j <= n; j++)
-            dp[0][j] = j;
+            prev[j] = j;
 
-        // Fill DP table
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+            curr[0] = i;
 
+            for (int j = 1; j <= n; j++) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 } else {
-                    dp[i][j] = 1 + min({
-                        dp[i - 1][j],     // Delete
-                        dp[i][j - 1],     // Insert
-                        dp[i - 1][j - 1]  // Replace
+                    curr[j] = 1 + min({
+                        prev[j],      // Delete
+                        curr[j - 1],  // Insert
+                        prev[j - 1]   // Replace
                     });
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
