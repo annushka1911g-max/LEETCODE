ecccbf808c5b9ecf4d5b1ee6de1e31fc8d9e98b4
Anushka Dwivedi <annushka1911g@gmail.com>
2026-09-06 12:21:22 +0530

    Time: 27 ms (71.17%), Space: 44.4 MB (20.6%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        vector<vector<unsigned long long>> dp(
+            m + 1,
+            vector<unsigned long long>(n + 1, 0)
+        );
+
+        // Empty t can always be formed in exactly one way
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // Don't use s[i-1]
+                dp[i][j] = dp[i - 1][j];
+
+                // Use s[i-1] if characters match
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
